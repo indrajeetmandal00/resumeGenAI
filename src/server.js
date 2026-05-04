462b9d0f64c8e62336c369cf1957b01cd45e9e64
Indrajeet <indrajeet20mandal02@gmail.com>
2026-05-04 10:51:48 +0530

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const app = require('./app');
+const connectDB = require('./config/db');
+
+connectDB();
+
+app.listen(3000,()=>{
+    console.log('server running in port 3000')
+})
